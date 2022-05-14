8b380bc58900f0a0bfd877a9e0a89e4b124e5b93
yuya-okada527 <yuya.okada527@gmail.com>
2022-05-14 15:12:11 +0900

    add erd

--- spike/lib/main.dart
+++ spike/lib/main.dart
@@ -10,11 +10,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Spike for treasure-box-app',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Spike for Treasure Box App'),
     );
   }
 }
