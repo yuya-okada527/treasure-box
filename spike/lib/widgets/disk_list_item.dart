c4c4b664f4d89fb6369a96bf35217fd67ad171de
yuya-okada527 <yuya.okada527@gmail.com>
2022-05-22 23:34:51 +0900

    adjust border radius

--- spike/lib/widgets/disk_list_item.dart
+++ spike/lib/widgets/disk_list_item.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:spike/models/disk_model.dart';
 
+const _borderRadius = 8.0;
+
 class DiskListItem extends StatelessWidget {
   final DiskModel disk;
   const DiskListItem({
@@ -12,15 +14,14 @@ class DiskListItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return Card(
       shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(10),
+        borderRadius: BorderRadius.circular(_borderRadius),
       ),
       child: Column(
         children: <Widget>[
           ClipRRect(
-            borderRadius: BorderRadius.circular(8.0),
+            borderRadius: BorderRadius.circular(_borderRadius),
             child: Image.asset("images/murmur.jpg", fit: BoxFit.fill),
           ),
-          // Image.asset("images/murmur.jpg", fit: BoxFit.fill),
           Text(disk.title ?? "Murmur")
         ],
       ),
